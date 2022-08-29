eda5494d2b3e2563e70d95aca786a4fca236ed91
Ali Hashemi <60406325+MR-AliHaashemi@users.noreply.github.com>
2022-08-30 03:57:51 +0430

    update(example)

--- example/main.go
+++ example/main.go
@@ -1,22 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"path/filepath"
 	"runtime"
 
 	"github.com/MR-AliHaashemi/fndl"
+	"github.com/er-azh/egmanifest"
 )
 
+var url = flag.String("url", "latest", "url of the manifest that you want to download the game")
+var dir = flag.String("dir", "Fortnite", "directory where you want to game stored")
+
+func init() { flag.Parse() }
+
 func main() {
-	pv, _ := fndl.NewManifestProvider()
-	inf, _ := pv.GetManifestInfo(fndl.Windows)
-	manifest, _ := pv.Download(inf)
+	var err error
+	var manifest *egmanifest.BinaryManifest
+	if *url == "latest" {
+		pv, _ := fndl.NewManifestProvider()
+		inf, _ := pv.GetManifestInfo(fndl.Windows)
+		manifest, err = pv.Download(inf)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		manifest, err = fndl.DownloadManifest(*url)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	downloader := fndl.NewDownloader(manifest, runtime.NumCPU()*2)
 
 	for _, file := range downloader.Files() {
-		path := filepath.Join("Fortnite", file.FileName)
+		path := filepath.Join(*dir, file.FileName)
 
 		if downloader.VerifyFile(file.FileName, path) {
 			fmt.Println("Verified: ", file.FileName)
